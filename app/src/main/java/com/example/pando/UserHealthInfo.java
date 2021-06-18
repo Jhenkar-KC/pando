2696eebc2663eaff7e0ba8f461f3da754927f4c6
Jhenkar-KC <73106302+Jhenkar-KC@users.noreply.github.com>
2021-06-18 16:27:22 +0530

    Initial Commit 04

--- app/src/main/java/com/example/pando/UserHealthInfo.java
+++ app/src/main/java/com/example/pando/UserHealthInfo.java
@@ -2,74 +2,51 @@ package com.example.pando;
 
 import com.google.firebase.database.IgnoreExtraProperties;
 
+import java.util.HashMap;
+
 @IgnoreExtraProperties
 public class UserHealthInfo {
 
-    private String username;
-    private String date_of_birth;
-    private String height;
-    private String weight;
-    private String last_period_date;
-    private String heart_rate;
-    private String blood_pressure;
+    private String UserName;
+    private String DateOfBirth;
+    private String Height;
+    private String Weight;
+    private String LastPeriodDate;
+    private String HeartRate;
+    private String BloodPressure;
 
+    public UserHealthInfo(String userName, String dateOfBirth, String height, String weight, String lastPeriodDate, String heartRate, String bloodPressure) {
+        UserName = userName;
+        DateOfBirth = dateOfBirth;
+        Height = height;
+        Weight = weight;
+        LastPeriodDate = lastPeriodDate;
+        HeartRate = heartRate;
+        BloodPressure = bloodPressure;
+    }
 
     public UserHealthInfo() {
-    }
 
-    public String getUsername() {
-        return username;
     }
-
-    public String getDate_of_birth() {
-        return date_of_birth;
-    }
-
-    public String getHeight() {
-        return height;
-    }
-
-    public String getWeight() {
-        return weight;
-    }
-
-    public String getLast_period_date() {
-        return last_period_date;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setDate_of_birth(String date_of_birth) {
-        this.date_of_birth = date_of_birth;
-    }
-
-    public void setHeight(String height) {
-        this.height = height;
-    }
-
-    public void setWeight(String weight) {
-        this.weight = weight;
-    }
-
-    public void setLast_period_date(String last_period_date) {
-        this.last_period_date = last_period_date;
-    }
-
-    public void setHeart_rate(String heart_rate) {
-        this.heart_rate = heart_rate;
-    }
-
-    public void setBlood_pressure(String blood_pressure) {
-        this.blood_pressure = blood_pressure;
-    }
-
-    public String getHeart_rate() {
-        return heart_rate;
-    }
-
     public String getBlood_pressure() {
-        return blood_pressure;
+        return BloodPressure;
+    }
+    public String getDate_of_birth() {
+        return DateOfBirth;
+    }
+    public String getHeart_rate() {
+        return HeartRate;
+    }
+    public String getHeight() {
+        return Height;
+    }
+    public String getLast_period_date() {
+        return LastPeriodDate;
+    }
+    public String getUsername() {
+        return UserName;
+    }
+    public String getWeight() {
+        return Weight;
     }
 }
--- app/src/main/java/com/example/pando/UserHealthInfoAdaptor.java
+++ app/src/main/java/com/example/pando/UserHealthInfoAdaptor.java
@@ -4,10 +4,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
 
 class UserHealthInfoAdapter  extends RecyclerView.Adapter<UserHealthInfoAdapter .UserHealthInfoViewHolder> {
@@ -16,7 +14,6 @@ class UserHealthInfoAdapter  extends RecyclerView.Adapter<UserHealthInfoAdapter 
     Context context;
 
     public UserHealthInfoAdapter (Context context , ArrayList<UserHealthInfo> UserHealthInfoList){
-
         this.UserHealthInfoList = UserHealthInfoList;
         this.context = context;
     }
@@ -26,19 +23,19 @@ class UserHealthInfoAdapter  extends RecyclerView.Adapter<UserHealthInfoAdapter 
     public UserHealthInfoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(context).inflate(R.layout.item , parent ,false);
         return new UserHealthInfoViewHolder(v);
-
     }
 
     @Override
     public void onBindViewHolder(@NonNull UserHealthInfoViewHolder holder, int position) {
         UserHealthInfo userHealthInfo = UserHealthInfoList.get(position);
-        holder.Name.setText(userHealthInfo.getUsername());
-        holder.Age.setText(userHealthInfo.getDate_of_birth());
-        holder.LastPeriodDate.setText(userHealthInfo.getLast_period_date());
-        holder.Weight.setText(userHealthInfo.getWeight());
-        holder.Height.setText(userHealthInfo.getUsername());
+
         holder.BloodPressure.setText(userHealthInfo.getBlood_pressure());
+        holder.Age.setText(userHealthInfo.getDate_of_birth());
         holder.HeartRate.setText(userHealthInfo.getHeart_rate());
+        holder.Height.setText(userHealthInfo.getHeight());
+        holder.LastPeriodDate.setText(userHealthInfo.getLast_period_date());
+        holder.UserName.setText(userHealthInfo.getUsername());
+        holder.Weight.setText(userHealthInfo.getWeight());
         holder.WarningPeriodDate.setText(userHealthInfo.getLast_period_date());
         holder.UpcomingPeriodDate.setText(userHealthInfo.getLast_period_date());
     }
@@ -48,22 +45,21 @@ class UserHealthInfoAdapter  extends RecyclerView.Adapter<UserHealthInfoAdapter 
         return UserHealthInfoList.size();
     }
 
-    public static  class UserHealthInfoViewHolder extends RecyclerView.ViewHolder{
+    public static class UserHealthInfoViewHolder extends RecyclerView.ViewHolder{
 
-        TextView Name,LastPeriodDate,UpcomingPeriodDate,WarningPeriodDate,Height,Weight,BloodPressure,HeartRate,Age;
+        TextView UserName,LastPeriodDate,UpcomingPeriodDate,WarningPeriodDate,Height,Weight,BloodPressure,HeartRate,Age;
 
         public UserHealthInfoViewHolder(@NonNull View textview) {
             super(textview);
-
-            Name = textview.findViewById(R.id.usernameCardView);
+            BloodPressure = textview.findViewById(R.id.blood_pressureCardView);
+            Age = textview.findViewById(R.id.ageCardView);
+            HeartRate = textview.findViewById(R.id.heart_rateCardView);
+            Height = textview.findViewById(R.id.heightCardView);
             LastPeriodDate = textview.findViewById(R.id.last_period_dateCardView);
+            UserName = textview.findViewById(R.id.usernameCardView);
+            Weight = textview.findViewById(R.id.weightCardView);
             UpcomingPeriodDate = textview.findViewById(R.id.upcomingPeriodDateCardView);
             WarningPeriodDate = textview.findViewById(R.id.warningPeriodDateCardView);
-            Height = textview.findViewById(R.id.heightCardView);
-            Weight = textview.findViewById(R.id.weightCardView);
-            BloodPressure = textview.findViewById(R.id.blood_pressureCardView);
-            HeartRate = textview.findViewById(R.id.heart_rateCardView);
-            Age = textview.findViewById(R.id.ageCardView);
         }
     }
 }
